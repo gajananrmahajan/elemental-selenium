dfc725e8417e64561623ee54950d4d3e443cb37c
Gajanan Mahajan <gajanan.forums@gmail.com>
2014-10-30 21:18:43 +0530

    Add test to upload file

---
+++ upload_file.rb
@@ -0,0 +1,33 @@
+# This script demonstrates how to upload a file without dealing with
+# dialog box and passing path of file to element to upload it.
+
+require 'selenium-webdriver'
+require 'rspec-expectations'
+include RSpec::Matchers
+
+def setup
+	@driver = Selenium::WebDriver.for :firefox
+	@driver.manage.window.maximize
+end
+
+def teardown
+	@driver.quit
+end
+
+def run
+	setup
+	yield
+	teardown
+end
+
+run do
+	file = "happy.jpg"
+	file_path = File.join(Dir.pwd + "/test_data", file).gsub("/", "\\\\")
+	@driver.get "http://the-internet.herokuapp.com/upload"
+	@driver.find_element(:id, "file-upload").send_keys "H:\\github\\elemental-selenium\\test_data\\happy.jpg"
+	@driver.find_element(:id, "file-submit").click
+
+	uploaded_file = @driver.find_element(:id, "uploaded-files").text
+	uploaded_file.should eql file
+end
+
